94a93345211d5e9626809e2a0e76fb9c086a5a29
sakitamako <sa.919.1025.ki@gmail.com>
2024-04-09 21:30:36 +0900

    コンストラクタ

--- src/review3/sakitamako/com/github/ColorChart.java
+++ src/review3/sakitamako/com/github/ColorChart.java
@@ -1,5 +1,13 @@
 package review3.sakitamako.com.github;
 
 public class ColorChart {
-
+	public String color = null;
+	public int level = 0;
+	
+	public ColorChart() {}
+	
+	public ColorChart(String color, int level) {
+		this.color = color;
+		this.level = level;
+	}
 }
--- src/review3/sakitamako/com/github/ColorTest.java
+++ src/review3/sakitamako/com/github/ColorTest.java
@@ -1,5 +1,18 @@
 package review3.sakitamako.com.github;
 
 public class ColorTest {
+	public static void main(String[] args) {
+		
+		ColorChart red = new ColorChart();
+		red.color = "赤";
+		red.level = 10;
+		System.out.println(red.color + "ベースの");
+		System.out.println(red.level + "トーンカラー");
+		
+		ColorChart blue = new ColorChart("青", 9);
+		System.out.println(blue.color + "ベースの");
+		System.out.println(blue.level + "トーンカラー");
+		
+	}
 
 }
